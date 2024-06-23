edc06e030f378c752b0d157c9b3107b6a6dcb4f2
Jonathan Freites <jonathanfreites@gmail.com>
2024-06-23 14:21:52 -0600

    chore: remove js scripts to avoid blocking

--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="es">
 
 <head>
@@ -29,30 +29,34 @@
 
 <body>
     <div>
-        <img src="/images/xvzoe-landing.jpg" alt="XV Zoe" usemap="#invitemap" {{-- <iframe width="1" height="1" wmode="transparent"
-            src="https://www.youtube.com/embed/e8jN1-HC-Vc?si=ThLVq2OwavjSlZOA" frameborder="0" autoplay loop></iframe> --}} </div>
+        <img src="/images/xvzoe-landing.jpg" alt="XV Zoe" usemap="#invitemap" />
+    </div>
 
-        <map name="invitemap">
-            <area target="_blank" alt="location" title="ubicación" href="https://maps.app.goo.gl/Ynpx9Kz7r1phZCQB9"
-                coords="335,2120,445,2230" shape="rect">
-            <area target="_blank" alt="Liverpool" title="Liverpool"
-                href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51456361" coords="500,2720,555,2782"
-                shape="rect">
-            <area target="_blank" alt="palacio de hierro" title="palacio de hierro"
-                href="https://www.elpalaciodehierro.com/buscar?eventId=387633" coords="500,2785,555,2830"
-                shape="rect">
-        </map>
+    <iframe width="1" height="1" wmode="transparent"
+        src="https://www.youtube-nocookie.com/embed/e8jN1-HC-Vc?si=GKOZeyaS5PqDooVr&amp;controls=0&amp;autoplay=1"
+        title="background music" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share"></iframe>
 
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/maphilight/1.4.2/jquery.maphilight.min.js"
-            integrity="sha512-1YiTT24MNHA6DRzyu+w9F5Egc8evYlyEnzSSTD4/M7q42xEb5fSpNgn0+1CPy3evubHs3xdlh8uXgae0DOhR7Q=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
-        <script>
-            // $(document).ready(function() {
-            //     $('img[usemap]').maphilight();
-            // });
-        </script>
+    <map name="invitemap">
+        <area target="_blank" alt="location" title="ubicación" href="https://maps.app.goo.gl/Ynpx9Kz7r1phZCQB9"
+            coords="335,2120,445,2230" shape="rect">
+        <area target="_blank" alt="Liverpool" title="Liverpool"
+            href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51456361" coords="500,2720,555,2782"
+            shape="rect">
+        <area target="_blank" alt="palacio de hierro" title="palacio de hierro"
+            href="https://www.elpalaciodehierro.com/buscar?eventId=387633" coords="500,2785,555,2830" shape="rect">
+    </map>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/maphilight/1.4.2/jquery.maphilight.min.js"
+                integrity="sha512-1YiTT24MNHA6DRzyu+w9F5Egc8evYlyEnzSSTD4/M7q42xEb5fSpNgn0+1CPy3evubHs3xdlh8uXgae0DOhR7Q=="
+                crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
+    {{-- <script>
+        $(document).ready(function() {
+            $('img[usemap]').maphilight();
+        });
+    </script> --}}
 </body>
 
 </html>
